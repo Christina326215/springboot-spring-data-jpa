d31058625c8543188f797d4954867763960277c8
Chtistina326215 <christina326215@gmail.com>
2025-04-14 17:25:26 +0800

    spring-data-jpa

---
+++ src/test/java/com/sunnyhsu/springbootspringdatajpa/SpringbootSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnyhsu.springbootspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
